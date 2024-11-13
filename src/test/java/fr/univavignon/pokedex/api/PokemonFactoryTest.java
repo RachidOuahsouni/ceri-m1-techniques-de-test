169500989e055096c6395832eab1da5227838493
rachidOuahsouni <rachidou286@gmail.com>
2024-11-13 14:23:23 +0100

    ajout de la classe test pour PokemonFactoryTest V2.

--- src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokemonFactoryTest.java
@@ -11,7 +11,7 @@ class PokemonFactoryTest {
         IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
         PokemonFactory factory = new PokemonFactory(metadataProvider);
 
-        // Test
+        // Test valide
         Pokemon pokemon = factory.createPokemon(0, 100, 100, 2000, 3);
 
         // Validation
@@ -25,5 +25,15 @@ class PokemonFactoryTest {
         assertEquals(100, pokemon.getHp());
         assertEquals(2000, pokemon.getDust());
         assertEquals(3, pokemon.getCandy());
+
+        // Test d'index invalide
+        Pokemon invalidPokemon = factory.createPokemon(-1, 100, 100, 2000, 3);
+
+        // Vérifie que l'index invalide retourne bien null
+        assertNull(invalidPokemon, "Pokemon creation with invalid index should return null");
+
+        // Test si l'exception est bien gérée (affichage de l'erreur dans la sortie standard)
+        factory.createPokemon(-1, 100, 100, 2000, 3);
+        fail("PokedexException should have been thrown");
     }
 }
